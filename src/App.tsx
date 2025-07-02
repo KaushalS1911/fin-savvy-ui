ccc3260faea9d20e64c047de21195ceed1155d9a
KaushalS1911 <darshilthummar01@gmail.com>
2025-07-02 17:11:00 +0530

    google tag manger added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Toaster} from "@/components/ui/toaster";
+import {Toaster as Sonner} from "@/components/ui/sonner";
+import {TooltipProvider} from "@/components/ui/tooltip";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Index from "./pages/Index";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
@@ -19,36 +19,38 @@ import {useEffect} from "react";
 
 const queryClient = new QueryClient();
 
-useEffect(() => {
-  const tagManagerArgs = {
-    gtmId: 'GTM-T8MC5FCJ'
-  };
-  TagManager.initialize(tagManagerArgs);
-}, []);
 
-const App = () => (
+const App = () => {
+    useEffect(() => {
+        const tagManagerArgs = {
+            gtmId: 'GTM-T8MC5FCJ'
+        };
+        TagManager.initialize(tagManagerArgs);
+    }, []);
+    return (
 
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/category/:categorySlug" element={<CategoryPosts />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/admin" element={<Admin />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+        <QueryClientProvider client={queryClient}>
+            <TooltipProvider>
+                <Toaster/>
+                <Sonner/>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Index/>}/>
+                        <Route path="/blog" element={<Blog/>}/>
+                        <Route path="/blog/:slug" element={<BlogPost/>}/>
+                        <Route path="/categories" element={<Categories/>}/>
+                        <Route path="/category/:categorySlug" element={<CategoryPosts/>}/>
+                        <Route path="/about" element={<About/>}/>
+                        <Route path="/contact" element={<Contact/>}/>
+                        <Route path="/search" element={<Search/>}/>
+                        <Route path="/admin" element={<Admin/>}/>
+                        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                        <Route path="*" element={<NotFound/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </TooltipProvider>
+        </QueryClientProvider>
+    )
+};
 
 export default App;
